3171c5fd00cd40f4da22ccf34bdecc02663b2ece
Oleksandr Ogniev <oleksandr.ogniev@norse.digital>
2023-10-18 13:26:41 +0300

    setup db

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
 export default function AuthCallbackPage() {
     const router = useRouter();
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
-
-    
+    trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({success}) => {
+            if (success) {
+                router.push(origin || '/dashboard');
+            }
+        },
+        onError: (err) => {
+            if (err.data?.code === "UNAUTHORIZED") {
+                router.push('/sign-in')
+            }
+        },
+        retry: true,
+        retryDelay: 500,
+    });
+    return (
+        <div className="w-full mt-24 justify-center flex ">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 animate-spin text-zinc-800"/>
+                <h3 className="font-semibold text-xl">Setting up your account...</h3>
+                <p>You will be redirecter automatically.</p>
+            </div>
+        </div>
+    );
 }
